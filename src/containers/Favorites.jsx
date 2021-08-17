fb47fadb966df9af07448ac6307f6fa7dbb0e789
dev-miguel-resources <ingenieromiguelch@gmail.com>
2021-08-16 21:21:04 -0400

    reducers, actions and store

---
+++ src/containers/Favorites.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+const Favorites = () => <p>Hello Am Favorites</p>;
+
+export default Favorites;
---
+++ src/index.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { compose } from "redux";
+import { createStore, applyMiddleware } from "redux";
+import logger from "redux-logger";
+import reducer from "./reducers";
+import App from "./routes/App";
+
+const initialState = {
+  favoriteCharacters: [],
+  sectionActive: "Characters",
+};
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+// store
+const store = createStore(
+  reducer,
+  initialState,
+  composeEnhancers(applyMiddleware(logger))
+);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
